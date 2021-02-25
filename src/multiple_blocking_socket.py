e7fb7f3dcddd9d6075f7896c98f747450be7b539
akitanak <akihito_tanaka@me.com>
2021-02-25 21:47:48 +0900

    add blocking socket app

---
+++ src/multiple_blocking_socket.py
@@ -0,0 +1,34 @@
+import socket
+
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = ("localhost", 8000)
+    server_socket.bind(address)
+    server_socket.listen()
+
+    connections = []
+
+    try:
+        while True:
+            conn, client_address = server_socket.accept()
+            print(f"I got a connection from {client_address}.")
+            connections.append(conn)
+
+            for conn in connections:
+                buffer = conn.recv(2)
+                print(f"I got data: {buffer}.")
+
+                while buffer[-2:] != b"\r\n":
+                    data = conn.recv(2)
+                    print(f"I got data: {data}")
+                    buffer = buffer + data
+
+                print(f"All data is {buffer}.")
+                conn.sendall(buffer)
+    finally:
+        server_socket.close()
+
+
+if __name__ == "__main__":
+    main()
