80b1e15948eee0c7e1cff36c4b5ddf3254fcef00
akitanak <akihito_tanaka@me.com>
2021-04-16 00:07:14 +0900

    RLockを使うことで同スレッドで再度同じlockを取得できる。

--- src/handling_blocking_work/recursion_with_locks.py
+++ src/handling_blocking_work/recursion_with_locks.py
@@ -1,7 +1,8 @@
-from threading import Lock, Thread
+from threading import Lock, RLock, Thread
 from typing import List
 
 list_lock = Lock()
+list_rlock = RLock()
 list = [1, 2, 3, 4]
 
 
@@ -11,7 +12,10 @@ def sum_list(int_list: List[int]) -> int:
     # 呼び出し元で取得したlockを開放せずに
     # 再起呼び出しした同関数内でlockを再び取得しようとしているので、
     # lockは永遠に取得できず処理を進めることができなくなってしまう。
-    with list_lock:
+    # with list_lock:
+    # RLock(reentrant lock)を使うことで、
+    # 同じスレッド内で２回以上lockを取得できるようになる。
+    with list_rlock:
         print("Acquired lock.")
         if len(int_list) == 1:
             print("Finished summing.")
