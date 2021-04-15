a07393def57223adc96c141b07194bfb829e2b1e
akitanak <akihito_tanaka@me.com>
2021-04-15 23:39:42 +0900

    add request count reporter with shared value.

--- src/handling_blocking_work/running_requests_with_threadpool.py
+++ src/handling_blocking_work/running_requests_with_threadpool.py
@@ -3,26 +3,43 @@ import functools
 import requests
 import sys
 from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
 
 sys.path.append("src")
 from util.async_timer import async_timed
 
 
+counter_lock = Lock()
+counter: int = 0
+
+
 def get_status_code(url: str) -> int:
+    global counter
     response = requests.get(url)
+    with counter_lock:
+        counter = counter + 1
     return response.status_code
+
+
+async def reporter(request_count: int):
+    while counter < request_count:
+        print(f"Finished {counter}/{request_count} requests")
+        await asyncio.sleep(0.5)
 
 
 @async_timed()
 async def main():
     loop = asyncio.get_running_loop()
     with ThreadPoolExecutor() as pool:
-        urls = ["https://www.example.com" for _ in range(1000)]
+        request_count = 1000
+        urls = ["https://www.example.com" for _ in range(request_count)]
+        reporter_task = asyncio.create_task(reporter(request_count))
         tasks = [
             loop.run_in_executor(pool, functools.partial(get_status_code, url))
             for url in urls
         ]
         results = await asyncio.gather(*tasks)
+        await reporter_task
         print(results)
 
 
